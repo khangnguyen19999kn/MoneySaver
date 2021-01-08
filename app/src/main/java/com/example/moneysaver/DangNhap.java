a2cb4205bff9aa63f58e5803d25f80e5e84ab11a
justnct <67194210+justnct@users.noreply.github.com>
2021-01-08 09:36:21 +0700

    dangnhap, dangky, chonnhom

--- app/src/main/java/com/example/moneysaver/DangNhap.java
+++ app/src/main/java/com/example/moneysaver/DangNhap.java
@@ -8,27 +8,40 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.moneysaver.model.Student;
+import com.example.moneysaver.model.User;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 public class DangNhap extends AppCompatActivity {
-    private EditText text_id,text_pass;
-    private Button button_login, button_register;
+    private TextInputEditText text_id,text_pass;
+    private TextInputLayout textInputLayoutId, textInputLayoutPass;
+    private Button button_login;
     private LoginButton loginButton;
     private CallbackManager callbackManager;
-    public SQLite sqLite;
+    private SQLite sqLite;
+    private TextView txtRegister;
+    private FirebaseDatabase database;
 
 
     @Override
@@ -37,54 +50,86 @@ public class DangNhap extends AppCompatActivity {
         setContentView(R.layout.activity_dang_nhap);
         //anh xa
 
-        text_id         = findViewById(R.id.textid);
-        text_pass       = findViewById(R.id.textpass);
-        button_login    = findViewById(R.id.buttonlogin);
-        loginButton     = findViewById(R.id.login_button);
-        button_register = findViewById(R.id.buttonregister);
-
-
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("message");
-        myRef.setValue("Hello, World!");
+        text_id             = (TextInputEditText) findViewById(R.id.textid);
+        text_pass           = (TextInputEditText) findViewById(R.id.textpass);
+        textInputLayoutId   = (TextInputLayout) findViewById(R.id.text_id_error);
+        textInputLayoutPass = (TextInputLayout) findViewById(R.id.text_pass_error);
+        button_login        = (Button) findViewById(R.id.buttonlogin);
+        loginButton         = (LoginButton) findViewById(R.id.login_button);
+        txtRegister         = (TextView) findViewById(R.id.txtregister);
+        final ArrayList<String> listString          =   new ArrayList<>();
+        final ArrayList<TextInputLayout>  listTextInputLayout =   new ArrayList<>();
+        listTextInputLayout.add(textInputLayoutId);
+        listTextInputLayout.add(textInputLayoutPass);
 
         //tao database
-        sqLite = new SQLite(this, "taikhoan.sqlite",null, 1);
-        sqLite.queryData("CREATE TABLE IF NOT EXISTS user(username VARCHAR(15),  pass VARCHAR(15), status INTEGER, IdVí VARCHAR(15))");
+//        sqLite = new SQLite(this, "taikhoan.sqlite",null, 1);
+//        sqLite.queryData("CREATE TABLE IF NOT EXISTS user1(username VARCHAR(15),  pass VARCHAR(15), status INTEGER, IdVí VARCHAR(15))");
+//        sqLite.queryData("INSERT INTO user1 VALUES ('nct','1234',1,'')");
 
-//        sqLite.queryData("INSERT INTO user VALUES ('nct','1234',1,'')");
         checkLogin();
 
         //setAction cho button_login
         button_login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String id = String.valueOf(text_id.getText());
-                String pass = String.valueOf(text_pass.getText());
-                if(TextUtils.isEmpty(id) || TextUtils.isEmpty(pass)){
-                    Toast.makeText(DangNhap.this, "Mày chưa nhập id hay pass nha tml !!!", Toast.LENGTH_SHORT).show();
-                }else{
-                    Cursor dataInformation = sqLite.getData("SELECT * FROM user WHERE username ='" + id + "' AND pass='" + pass + "'");
-                    if(dataInformation != null) {
-                        while (dataInformation.moveToNext()){
-                            Intent it_nav = new Intent(DangNhap.this, FirstPage.class);
-                            it_nav.putExtra("name",dataInformation.getString(1));
-                            sqLite.queryData("UPDATE user SET status = 2 where username ='" + dataInformation.getString(0) + "'");
-                            startActivity(it_nav);
-                        }
-                    }else{
-                        Toast.makeText(DangNhap.this, "Sai thông tin !!!", Toast.LENGTH_SHORT).show();
+                for (int i = 0 ; i < listTextInputLayout.size(); i++){
+                    listTextInputLayout.get(i).setError(null);
+                }
+                final String id = String.valueOf(text_id.getText()).trim();
+                final String pass = String.valueOf(text_pass.getText()).trim();
+                int count = 0;
+                listString.clear();
+                listString.add(id);
+                listString.add(pass);
+
+                for (int i = 0; i < 2; i++) {
+                    if(TextUtils.isEmpty(listString.get(i))){
+                        listTextInputLayout.get(i).setError("Bạn chưa nhập trường này !");
+                    }else if (!TextUtils.isEmpty(listString.get(i))){
+                        count++;
+                        listTextInputLayout.get(i).setError(null);
                     }
+                }
+
+                if (count == listString.size()){
+                    final boolean[] success = {false};
+                    final DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("User");
+                    reference.addValueEventListener(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                            for(DataSnapshot dataSnapshot : snapshot.getChildren()){
+                                User user = (User) dataSnapshot.getValue(User.class);
+                                if(user.getId().equals(id) && user.getPass().equals(pass)) {
+                                    Intent it_nav = new Intent(DangNhap.this, FirstPage.class);
+                                    User changeStatusUser = new User(user.getId(), user.getPass(), 2, "");
+                                    reference.child(user.getId()).setValue(changeStatusUser);
+                                    success[0] = true;
+                                    startActivity(it_nav);
+                                    finish();
+                                    break;
+                                }
+                            }
+                            if(!success[0]){
+                                Toast.makeText(DangNhap.this, "Sai thông tin ", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                        }
+                    });
                 }
             }
         });
 
-        //buttonLogin
-        button_register.setOnClickListener(new View.OnClickListener() {
+        //setAction textview tao tai khoan moi
+        txtRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent it_register = new Intent(DangNhap.this, DangKy.class);
                 startActivity(it_register);
+                finish();
             }
         });
 
@@ -96,6 +141,7 @@ public class DangNhap extends AppCompatActivity {
             public void onSuccess(LoginResult loginResult) {
                 Intent intent = new Intent(DangNhap.this, FirstPage.class);
                 startActivity(intent);
+                finish();
             }
 
             @Override
@@ -118,10 +164,25 @@ public class DangNhap extends AppCompatActivity {
     }
 
     public void checkLogin(){
-        Cursor getAccLogin = sqLite.getData("SELECT * FROM user WHERE status = 2");
-        if(getAccLogin != null){
-            Intent i = new Intent(DangNhap.this, FirstPage.class);
-            startActivity(i);
-        }
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference().child("User");
+        reference.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot dataSnapshot : snapshot.getChildren()){
+                    User user = (User) dataSnapshot.getValue(User.class);
+                    if(user.getStatus() == 2) {
+                        Intent it_nav = new Intent(DangNhap.this, FirstPage.class);
+                        startActivity(it_nav);
+                        finish();
+                        break;
+                    }
+                }
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
+
 }
