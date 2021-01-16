e6c6586de06c414e3b1b2e3ea1750438a3d9ce13
Nghia <nghia02584@gmail.com>
2021-01-16 22:35:09 +0700

    khi thêm giao dịch dữ liêu được lưu vào firebase

--- app/src/main/java/com/example/moneysaver/lapkehoach/SuKienActivity.java
+++ app/src/main/java/com/example/moneysaver/lapkehoach/SuKienActivity.java
@@ -1,13 +1,23 @@
 package com.example.moneysaver.lapkehoach;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.moneysaver.R;
 import com.example.moneysaver.model.Student;
+import com.google.firebase.FirebaseError;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.MutableData;
+import com.google.firebase.database.Transaction;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class SuKienActivity extends AppCompatActivity {
 
@@ -16,12 +26,18 @@ public class SuKienActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_su_kien);
 
-        FirebaseDatabase database = FirebaseDatabase.getInstance();
-        DatabaseReference myRef = database.getReference("Student");
+//        FirebaseDatabase database = FirebaseDatabase.getInstance();
+//        DatabaseReference myRef = database.getReference("Student");
         Student student = new Student("Nguyễn Tấn Nghĩa",23);
-        myRef.child("students").child("17130133").setValue(student);
+//        myRef.child("17130133").setValue(student);
+        incrementCounter(student);
     }
     private void writeNewStudent(String name,int age){
+
+    }
+    public void incrementCounter(final Student student) {
+
+
 
     }
 }
