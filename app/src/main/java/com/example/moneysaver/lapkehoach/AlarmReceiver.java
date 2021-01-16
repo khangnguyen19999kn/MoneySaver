534f75930a44975c392342c25fd4d1c876ddc519
TruongTra <nhattruong25041999@gmail.com>
2021-01-17 00:05:40 +0700

    ThemKeHoach

---
+++ app/src/main/java/com/example/moneysaver/lapkehoach/AlarmReceiver.java
@@ -0,0 +1,10 @@
+
+package com.example.moneysaver.lapkehoach;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+public class AlarmReceiver extends BroadcastReceiver {
+    @Override
+    public void onReceive(Context context, Intent intent) {
+    }
+}
--- app/src/main/java/com/example/moneysaver/lapkehoach/SuKienActivity.java
+++ app/src/main/java/com/example/moneysaver/lapkehoach/SuKienActivity.java
@@ -1,43 +1,58 @@
 package com.example.moneysaver.lapkehoach;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.moneysaver.ChonNhom;
+import com.example.moneysaver.FirstPage;
 import com.example.moneysaver.R;
+import com.example.moneysaver.ThemKeHoach;
 import com.example.moneysaver.model.Student;
-import com.google.firebase.FirebaseError;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.MutableData;
-import com.google.firebase.database.Transaction;
-import com.google.firebase.database.ValueEventListener;
-
-import java.util.ArrayList;
 
 public class SuKienActivity extends AppCompatActivity {
+    Button btnThemSuKien;
+    Button btnVeSoGiaoDich;
+    private static final int REQUEST_CODE_EXAMPLE = 0x9345;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_su_kien);
 
-//        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
 //        DatabaseReference myRef = database.getReference("Student");
-        Student student = new Student("Nguyễn Tấn Nghĩa",23);
-//        myRef.child("17130133").setValue(student);
-        incrementCounter(student);
+//        Student student = new Student("Nguyễn Tấn Nghĩa",23);
+//        myRef.child("students").child("17130133").setValue(student);
+
+        btnThemSuKien = findViewById(R.id.btnThemSuKien);
+        btnVeSoGiaoDich = findViewById(R.id.btnVeSoGiaoDich);
+        btnThemSuKien.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                v.startAnimation(AnimationUtils.loadAnimation(SuKienActivity.this,R.anim.anim_click));
+                Intent intent  = new Intent(SuKienActivity.this, ThemKeHoach.class);
+                startActivityForResult(intent, REQUEST_CODE_EXAMPLE);
+            }
+        });
+        btnVeSoGiaoDich.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                v.startAnimation(AnimationUtils.loadAnimation(SuKienActivity.this,R.anim.anim_click));
+                Intent intent  = new Intent(SuKienActivity.this, FirstPage.class);
+                startActivityForResult(intent, REQUEST_CODE_EXAMPLE);
+            }
+        });
     }
     private void writeNewStudent(String name,int age){
-
-    }
-    public void incrementCounter(final Student student) {
-
-
 
     }
 }
