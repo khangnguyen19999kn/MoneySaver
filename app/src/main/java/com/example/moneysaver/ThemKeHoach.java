8154ecdc87deb6fd8898fa2c84a52fa8445bbbbc
TruongTra <nhattruong25041999@gmail.com>
2021-01-17 23:21:54 +0700

    scheduling

--- app/src/main/java/com/example/moneysaver/ThemKeHoach.java
+++ app/src/main/java/com/example/moneysaver/ThemKeHoach.java
@@ -1,5 +1,9 @@
 package com.example.moneysaver;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +12,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import com.example.moneysaver.datasource.ChiTieuDataSource;
+import com.example.moneysaver.lapkehoach.AlarmReceiver;
 import com.example.moneysaver.lapkehoach.SuKienActivity;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
@@ -17,10 +22,19 @@ public class ThemKeHoach extends AppCompatActivity {
     TextView chonNhomKH,ngayKH;
     private ChiTieuDataSource chiTieuDataSource;
     private static final int REQUEST_CODE_EXAMPLE = 0x9345;
+    AlarmManager alarmManager;
+    PendingIntent pendingIntent;
+    Calendar calendar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_them_ke_hoach);
+
+        final Intent intentAlamr = new Intent(ThemKeHoach.this, AlarmReceiver.class);
+        alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        final Intent intent = new Intent(ThemKeHoach.this, FirstPage.class);
+
         btnThemKH = findViewById(R.id.btnThemKH);
         tienKH = findViewById(R.id.nhapTienKH);
         ghiChuKH = findViewById(R.id.noteKH);
@@ -31,14 +45,22 @@ public class ThemKeHoach extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 //get data from view
-                //insert data
-                chiTieuDataSource = new ChiTieuDataSource(ThemKeHoach.this);
-                chiTieuDataSource.createChiTieu(1001,Integer.parseInt(tienKH.getText().toString()),
-                        ngayKH.getText().toString(),ghiChuKH.getText().toString(),"user1vi1");
+//                insertData();
+
+                //
+                intentAlamr.putExtra("tien",tienKH.getText().toString());
+                intentAlamr.putExtra("ngay",ngayKH.getText().toString());
+                intentAlamr.putExtra("ghichu",ghiChuKH.getText().toString());
+                pendingIntent = PendingIntent.getBroadcast(
+                        ThemKeHoach.this,0,intentAlamr,PendingIntent.FLAG_UPDATE_CURRENT
+                );
+                calendar = Calendar.getInstance();
+                alarmManager.set(AlarmManager.RTC_WAKEUP,calendar.getTimeInMillis()+60000,pendingIntent);
 
                 //
                 v.startAnimation(AnimationUtils.loadAnimation(ThemKeHoach.this,R.anim.anim_click));
-                Intent intent  = new Intent(ThemKeHoach.this, SuKienActivity.class);
+                Intent intent  = new Intent(ThemKeHoach.this, FirstPage.class);
+
                 startActivity(intent);
             }
         });
@@ -50,18 +72,27 @@ public class ThemKeHoach extends AppCompatActivity {
                 startActivityForResult(intent, REQUEST_CODE_EXAMPLE);
             }
         });
-        ngayKH.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                v.startAnimation(AnimationUtils.loadAnimation(ThemKeHoach.this,R.anim.anim_click));
-                Intent intent  = new Intent(ThemKeHoach.this, ActivityDate.class);
-                startActivity(intent);
-            }
-        });
-    }private String getDate(){
+
+    }
+
+    private String getDate(){
         Calendar calendar = Calendar.getInstance();
         SimpleDateFormat dinhDangNgay = new SimpleDateFormat("dd/MM/yyyy");
         String ngay = dinhDangNgay.format(calendar.getTime());
         return ngay;
     }
+    
+    public void insertData(){
+
+                chiTieuDataSource = new ChiTieuDataSource(ThemKeHoach.this);
+                chiTieuDataSource.createChiTieu(1001,Integer.parseInt(tienKH.getText().toString()),
+                        ngayKH.getText().toString(),ghiChuKH.getText().toString(),"user1vi1");
+
+    }
+//    private void scheduleNotification(Context context, long time, int idToDo) {
+//        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+//        PendingIntent pendingIntent = IntentFactory.getNotificationPendingIntent(this, "ToDo", "DemoToDo");
+//        alarmMng.set(AlarmManager.RTC_WAKEUP, time, pendingIntent);
+//    }
+
 }
--- app/src/main/java/com/example/moneysaver/lapkehoach/AlarmReceiver.java
+++ app/src/main/java/com/example/moneysaver/lapkehoach/AlarmReceiver.java
@@ -3,8 +3,25 @@ package com.example.moneysaver.lapkehoach;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+
+import com.example.moneysaver.FirstPage;
+import com.example.moneysaver.ThemKeHoach;
+import com.example.moneysaver.datasource.ChiTieuDataSource;
+
 public class AlarmReceiver extends BroadcastReceiver {
+    private ChiTieuDataSource chiTieuDataSource;
     @Override
     public void onReceive(Context context, Intent intent) {
+        chiTieuDataSource = new ChiTieuDataSource(context);
+        int tien = Integer.parseInt(intent.getExtras().getString("tien"));
+        String ngay = intent.getExtras().getString("ngay");
+        String ghiChu = intent.getExtras().getString("ghichu");
+        intent.getExtras().getString("ngay");
+        intent.getExtras().getString("ghichu");
+        chiTieuDataSource.createChiTieu(1001,tien,
+                ngay,ghiChu,"user1vi1");
+//        Intent intent2 = new Intent(AlarmReceiver.this, FirstPage.class);
+
+
     }
 }
